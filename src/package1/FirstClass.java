7bc7db4e41be925ba3d7bdee57d4b8d0caf242fb
sharopovbehzod <sharopovbehzod@gmail.com>
2021-03-13 14:31:55 -0500

    second commit

---
+++ src/package1/FirstClass.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		/*
+		 * git add .
+           git status
+           git commit -m "first commit"
+           git remote add origin ​ (copy it from your gitHub)
+           git push -u origin master (or main)
+		 */
+
+	}
+
+}
